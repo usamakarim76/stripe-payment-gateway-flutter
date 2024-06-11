22c78698ad83c43de6ce49f66c089a509d226e8e
Usama <usamakarim076@gmail.com>
2024-06-11 10:32:17 +0500

    Updated

--- android/app/src/main/kotlin/com/example/stripe_payment_gateway/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stripe_payment_gateway/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.stripe_payment_gateway
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
