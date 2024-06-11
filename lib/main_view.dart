9fb4d43ad81094413483756a294068c2c9b0a5ca
Usama <usamakarim076@gmail.com>
2024-06-11 11:29:10 +0500

    Update

---
+++ lib/main_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text("Pay"),
+        ),
+      )),
+    );
+  }
+}
